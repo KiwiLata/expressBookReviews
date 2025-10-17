548f72ba22c88312d7cce06a9b8a7c42b38eb3fe
Anna Abramek <coffeeplant@abramek.art>
2025-10-17 03:07:34 -0400

    add search for book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,13 +18,21 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   let isbn = req.params.isbn;
-  res.send(books[isbn]);
+  let book = books[isbn];
+  res.send(JSON.stringify(book, null , 3));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let book;
+
+  for(let isbn in books) {
+    if(books[isbn].author === author) {
+        book = books[isbn];
+    }
+  }
+  res.send(JSON.stringify(book, null , 3));
 });
 
 // Get all books based on title
